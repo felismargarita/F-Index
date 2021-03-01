c022e2b7fcd4c39d71f5a4dc2021b8d803452561
felis <felismargarita@hotmail.com>
2021-03-01 15:49:14 +0800

    增加 cloud music app的导航

--- src/pages/Collections.tsx
+++ src/pages/Collections.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import IndexCard from '@/components/IndexCard'
 import ichatImg from '@/assets/imgs/ichat.jpg'
+import cloudmusic from '@/assets/imgs/cloudmusic.jpg'
+
 const Collections = ()=>{
   return (
     <div className="index">
       <IndexCard title="wechat" img={ichatImg} type="site" link="/ichat" />
+      <IndexCard title="cloud music" img={cloudmusic} type="site" link="/cloudmusic" />
     </div>
   )
 }
